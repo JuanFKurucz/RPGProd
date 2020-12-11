7aa8a95736a6324b96a36ca2c1ca6db7b35f597f
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2020-12-11 18:09:24 -0300

    chore: add linting to project

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
